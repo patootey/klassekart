4ede0f86b4ce0fb894c22b16ca5c8950b986861a
Grocto <groctoz@gmail.com>
2023-08-31 12:29:14 +0200

    eksmeplel

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from tkinter import *
+import tkinter as tk
+import general as ge
+from PIL import Image, ImageTk
+import test
 
-print("test")
-root = Tk()
+r = tk.Tk()
+r.geometry("400x300")
 
-# rest
+
+def main(root):
+    ge.prevpage = ge.keep_page(root)
+    ge.clear_window(root)
+    button = ge.Button(root, text="Click Me", command=lambda: test.wowow(root))
+    root.mainloop()
+
+home_button = ge.Photo(r, "./images/home.png", size=(40,40),position=(40,0) ,bind=lambda: main(r))
+arrow = ge.Photo(r, "./images/arrow.png", size=(40,40), bind=lambda: ge.load_page(r, ge.prevpage))
+ge.save_widget(arrow.label)
+ge.save_widget(home_button.label)
+main(r)
