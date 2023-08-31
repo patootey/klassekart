be7f238e608212e0fcd555b080677b7a208e4df8
Mathilde Borsodi Hegna <mathilde.hegna@gmail.com>
2023-08-31 12:41:43 +0200

    changes

--- test.py
+++ test.py
@@ -4,12 +4,16 @@ import general as ge
 
 def wowow(root):
     ge.prevpage = ge.keep_page(root)
+
     def key_pressed(event):
         print(ge.prevpage)
 
     # Bind the key event to the function
     root.bind("<KeyPress-A>", key_pressed)
 
-    button = ge.Button(root, text="Tilbake", colour="red", command=lambda: ge.load_page(root, ge.prevpage))
-
-
+    button = ge.Button(
+        root,
+        text="Tilbake",
+        colour="red",
+        command=lambda: ge.load_page(root, ge.prevpage),
+    )
